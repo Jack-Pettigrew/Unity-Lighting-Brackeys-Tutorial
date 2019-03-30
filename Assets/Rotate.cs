52a5349681ed78a1c47eaf3e04daafd8c44053a3
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-03-30 11:22:37 +0000

    Initial commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private static List<GameObject> gameObjects = new List<GameObject>(3);
+
+    [SerializeField, Header("Camera Movement"), 
+        Tooltip("The speed which the camera is turned.")]
+    private float _turnSpeed = 1.0f;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        this.transform.Rotate(0, Time.fixedDeltaTime * _turnSpeed, 0);
+	}
+}
